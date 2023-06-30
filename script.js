8ba49f500c8fc9610bf192a0dc786fa7d0def53e
Swarup Mahato <115489346+swarupinfotech@users.noreply.github.com>
2023-06-30 17:28:25 +0530

    feedback-form

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggle_visibility() {
+    var e = document.getElementById('feedback-main');
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
